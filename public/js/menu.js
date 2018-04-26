c17568e44c516d2860a58f222a603744a00ed8fc
Fabien Grondin <0fabien0@gmail.com>
2018-04-26 16:13:43 +0200

    Add menu header

---
+++ public/js/menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  if (location.search == "") {
+    $('a[href="index.php"]').parent().addClass('active');
+  }
+  else if (location.search == "?action=listPosts") {
+    $('a[href="index.php?action=listPosts"]').parent().addClass('active');
+  }
+});
