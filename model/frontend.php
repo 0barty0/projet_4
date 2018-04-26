06e70e1c7f16f189839ec9a45fb6fabc9c7713f8
Fabien Grondin <0fabien0@gmail.com>
2018-04-26 14:39:34 +0200

    Add post except for list posts

--- model/frontend.php
+++ model/frontend.php
@@ -3,7 +3,7 @@ function getPosts()
 {
     $db = dbConnect();
 
-    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC');
+    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date');
 
     return $req;
 }
--- view/frontend/lists_posts.php
+++ view/frontend/lists_posts.php
@@ -6,15 +6,21 @@ ob_start();
 <h1>Le fameux livre</h1>
 <?php
   while ($post=$req->fetch()) {
-      ?>
+      $content = $post['content'];
+      if (preg_match('/^.{1,250}\b/su', $content, $match)) {
+          $except = $match[0];
+      } ?>
       <div class="row">
         <div class="panel-group col-md-8 col-md-offset-2">
-          <div class="panel panel-default">
+          <div class="panel panel-primary">
             <div class="panel-heading">
               <h2><?= $post['title'] ?></h2>
             </div>
             <div class="panel-body">
-              <p><?= $post['content'] ?></p>
+              <p><?= nl2br($except) ?> ...</p>
+              <div class="text-right">
+                <a href="#" class="btn btn-primary">Lire la suite</a>
+              </div>
             </div>
           </div>
         </div>
