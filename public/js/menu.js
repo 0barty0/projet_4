25e912fe8597650535ec647d507e0a36664668c8
Fabien Grondin <0fabien0@gmail.com>
2018-04-27 11:42:46 +0200

    Menu highlight for post pages

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-
-  if (location.search == "") {
+  let action = location.search;
+  if (action == "") {
     $('a[href="index.php"]').parent().addClass('active');
   }
-  else if (location.search == "?action=listPosts") {
+  else if (action == "?action=listPosts" || action.search(/^\?action=post/g) !== -1) {
     $('a[href="index.php?action=listPosts"]').parent().addClass('active');
-    
   }
 });
