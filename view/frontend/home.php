8abb12e19a056498d5353a84dba622d0e9c8db43
Fabien Grondin <0fabien0@gmail.com>
2018-05-10 18:10:32 +0200

    Add presentation and footer div

--- view/frontend/home.php
+++ view/frontend/home.php
@@ -3,12 +3,22 @@ $title = "Un billet pour l'Alaska";
 
 ob_start();
 ?>
-  <div class="row" id="home">
+  <div class="row">
     <div class="col-lg-12">
-      <img src="public/images/alaska_mountains.jpg" alt="Montagne d'Alaska" id="home_image">
-      <div id="home_title">
-        <h1 class="display-2">Un billet pour l'Alaska</h1>
-        <h2 class="text-right">Le nouveau livre de Jean Forteroche</h2>
+      <div id="home">
+        <img src="public/images/alaska_mountains.jpg" alt="Montagne d'Alaska" id="home_image">
+        <div id="home_title">
+          <h1 class="display-2">Un billet pour l'Alaska</h1>
+          <h2 class="text-right">Le nouveau livre de Jean Forteroche</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12">
+      <div id="presentation">
+        <p>L'écrivain et acteur Jean Forteroche vous propose de découvrir son dernier roman au fil de sa création.</p>
+        <p>Il le publiera sur son blog chapître après chapître.</p>
       </div>
     </div>
   </div>
