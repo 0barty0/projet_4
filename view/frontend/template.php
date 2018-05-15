1f2268c088d8652f42564400b77681a6331180a3
Fabien Grondin <0fabien0@gmail.com>
2018-05-15 10:41:25 +0200

    Add meta-tags

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -2,6 +2,9 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
+    <meta name="description" content="Un billet pour l'alaska, roman de Jean Forteroche">
+    <meta name="keywords" content="Jean Forteroche,alaska,roman,blog">
+    <meta name="author" content="Jean Forteroche">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
